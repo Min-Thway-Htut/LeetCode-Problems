cd05dcd127459b5e91c18709a6f1760483fc86ca
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-09-08 16:24:32 +0200

    Solution to square root problem

--- Easy/sqrt.js
+++ Easy/sqrt.js
@@ -1,6 +1,20 @@
-function squaredNumber(number){
-    const finalNumber = number ** 0.5;
-    console.log(finalNumber);
-}
+function mySqrt(x) {
+    if (x < 2) {
+        return x;
+    }
 
-squaredNumber(9);
+    let left = 0;
+    let right = x;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (mid * mid === x) {
+            return mid;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return right;
+}
