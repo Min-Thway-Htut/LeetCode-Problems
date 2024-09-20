d78ecafde8e873b2642be11fb3944d10f7c02738
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-09-20 23:38:54 +0200

    Solution to LC2278

---
+++ Easy/lc2278.py
@@ -0,0 +1,5 @@
+class Solution:
+    def percentageLetter(self, s: str, letter: str) -> int:
+        count = s.count(letter)
+        final_result = (count / len(s)) * 100
+        return math.floor(final_result)
