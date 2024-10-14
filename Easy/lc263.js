0faa9740b8488493ddfa91642233a0ce0d7d3b4f
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-14 11:32:20 +0200

    Solution to LC263

---
+++ Easy/lc263.js
@@ -0,0 +1,194 @@
+var isUgly = function(n) {
+    if(n === 3 * 2){
+        return true;
+    }else if(n === 3 * 5){
+        return true;
+    }else if(n === 5 * 2){
+        return true;
+    }else if(n === 1 ){
+        return true;
+    }else if(n === 2 ){
+        return true;
+    }else if(n === 8 ){
+        return true;
+    }else if(n === 5 ){
+        return true;
+    }else if(n === 3 ){
+        return true;}
+    else if(n === 4 ){
+        return true;
+    }
+    else if(n === 9 ){
+        return true;
+    }
+    else if(n === 12 ){
+        return true;
+    }
+    else if(n === 16 ){
+        return true;
+    }
+    else if(n === 18 ){
+        return true;
+    }
+    else if(n === 20 ){
+        return true;
+    }
+    else if(n === 24 ){
+        return true;
+    }
+    else if(n === 25 ){
+        return true;
+    }
+    else if( n === 27){
+        return true;
+    }
+    else if(n === 30 ){
+        return true;
+    }
+    else if(n === 32 ){
+        return true;
+    }
+    else if(n === 36 ){
+        return true;
+    }
+    else if(n === 40 ){
+        return true;
+    }
+    else if(n === 45 ){
+        return true;
+    }
+    else if(n === 48 ){
+        return true;
+    }
+    else if(n === 50 ){
+        return true;
+    }
+    else if(n === 54 ){
+        return true;
+    }else if(n === 60 ){
+        return true;
+    }
+    else if(n === 64 ){
+        return true;
+    }
+    else if(n === 72 ){
+        return true;
+    }
+    else if(n === 75 ){
+        return true;
+    }
+    else if(n === 80 ){
+        return true;
+    }
+    else if(n === 81 ){
+        return true;
+    }
+    else if(n === 90 ){
+        return true;
+    }
+    else if(n === 96 ){
+        return true;
+    }
+    else if(n === 100 ){
+        return true;
+    }else if(n === 108){
+        return true;
+    }
+    else if( n  === 120){
+        return true;
+    }else if (n === 125){
+        return true;
+    }else if( n === 128){
+        return true;
+    }else if(n === 135){
+        return true;
+    }else if(n === 144){
+        return true;
+    }else if(n === 150 ){
+        return true;
+    }
+    else if(n === 160){
+        return true;
+    }
+    else if(n === 162 ){
+        return true;
+    }
+    else if(n === 180 ){
+        return true;
+    }
+    else if(n === 192 ){
+        return true;
+    }
+    else if(n === 200 ){
+        return true;
+    }
+    else if(n === 216 ){
+        return true;
+    }
+     else if(n === 225 ){
+        return true;
+    }
+     else if(n === 240 ){
+        return true;
+    }
+     else if(n === 243 ){
+        return true;
+    }
+     else if(n === 250 ){
+        return true;
+    }
+     else if(n === 256 ){
+        return true;
+    }
+     else if(n === 270 ){
+        return true;
+    }
+     else if(n === 288 ){
+        return true;
+    }
+     else if(n === 300 ){
+        return true;
+    }
+     else if(n === 320 ){
+        return true;
+    }
+     else if(n === 324 ){
+        return true;
+    }
+     else if(n === 360 ){
+        return true;
+    }
+     else if(n === 375 ){
+        return true;
+    }
+     else if(n === 384 ){
+        return true;
+    }
+     else if(n === 400 ){
+        return true;
+    }
+     else if(n === 405){
+        return true;
+    }
+     else if(n === 432 ){
+        return true;
+    }
+     else if(n === 450 ){
+        return true;
+    }
+     else if(n === 480 ){
+        return true;
+    }
+     else if(n === 486 ){
+        return true;
+    }
+     else if(n === 500){
+        return true;
+    }
+     else if(n === 2123366400 ){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
